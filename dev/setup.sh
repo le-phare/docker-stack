7e18e19bb1bb229ce6626d83d43361bee03113ef
Nicolas LAURENT <nicolas.laurent@lephare.com>
2022-05-12 19:19:17 +0200

    fixup! feat(networks): update networks to use only two

---
+++ dev/setup.sh
@@ -0,0 +1,13 @@
+#!sh
+
+docker compose --ansi never up -d --build 2>& 1> /dev/null
+docker compose cp proxy:/usr/bin/caddy /tmp/caddy
+
+XDG_DATA_HOME=/var/lib/docker/volumes/dev_caddy_data/_data
+
+sudo XDG_DATA_HOME=${XDG_DATA_HOME} \
+/tmp/caddy start 2&> /dev/null; \
+/tmp/caddy trust; \
+/tmp/caddy stop 2&> /dev/null; \
+
+rm /tmp/caddy
