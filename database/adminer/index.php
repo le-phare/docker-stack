0778dc523025badff8a7f71ebb0c81e31535fc50
Lucas Mirloup <97196064+lucasmirloup@users.noreply.github.com>
2024-08-30 10:12:20 +0200

    feat(adminer): switch to Adminer Evo, pre-login to database servers (#32)

---
+++ database/adminer/index.php
@@ -0,0 +1,31 @@
+<?php
+
+function adminer_object()
+{
+  // required to run any plugin
+  include_once "./plugin.php";
+
+  // autoloader
+  foreach (glob("plugins/*.php") as $filename) {
+    include_once "./$filename";
+  }
+
+  // enable extra drivers just by including them
+  //~ include "./plugins/drivers/simpledb.php";
+
+  $plugins = array(
+    // specify enabled plugins here
+    new LePharePluginPreLoginToDatabases(),
+  );
+
+  /* It is possible to combine customization and plugins:
+  class AdminerCustomization extends AdminerPlugin {
+  }
+  return new AdminerCustomization($plugins);
+  */
+
+  return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer.php";
---
+++ database/adminer/plugins/le-phare-pre-login-to-databases.php
@@ -0,0 +1,21 @@
+<?php
+
+/** Pre-login to Le Phare database servers */
+class LePharePluginPreLoginToDatabases
+{
+  function __construct()
+  {
+    set_password('server', 'mysql_5', 'root', 'root');
+
+    set_password('server', 'mariadb_10', 'root', 'root');
+
+    set_password('pgsql', 'pgsql_9', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_10', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_11', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_12', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_13', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_14', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_15', 'postgres', 'root');
+    set_password('pgsql', 'pgsql_16', 'postgres', 'root');
+  }
+}
